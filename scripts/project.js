f313bad9242b36b0b25bd1ea1e2bea561fc86db6
Katie Diggory <Katie.diggory@gmail.com>
2021-12-13 08:59:00 +0000

    GAR-7 #comment Adding show form for name DOM

--- scripts/project.js
+++ scripts/project.js
@@ -198,6 +198,76 @@ const readByName = () => {
     showName();
 }
 
+const readByMonth = () => {
+    console.log("in the read by planting month function");
+    document.querySelector("#doingWhat").textContent = "Please enter the planting month of the plants you would like to find";
+    showMonth();
+}
+
+const showMonth = () => {
+    const div =   document.querySelector(".formContainer");
+    while(div.firstChild){
+        div.removeChild(div.firstChild);
+    }
+    const monthInputLabel = document.createElement("label");
+  monthInputLabel.setAttribute("for", "month");
+  monthInputLabel.setAttribute("class", "label");
+  monthInputLabel.innerText = "Planting month:";
+  document.querySelector(".formContainer").appendChild(monthInputLabel);
+  document.querySelector(".formContainer").appendChild(document.createElement("br"));
+  const monthInput = document.createElement("input");
+  monthInput.setAttribute("type", "text");
+  monthInput.setAttribute("class", "input");
+  monthInput.setAttribute("id", "monthActual");
+  monthInput.setAttribute("name", "month");
+  document.querySelector(".formContainer").appendChild(monthInput);
+  document.querySelector(".formContainer").appendChild(document.createElement("br"));
+
+  const subBut = document.createElement("button");
+  subBut.setAttribute("class", "formButton");
+  subBut.setAttribute("id", "submit");
+  subBut.innerText = "Submit";
+  document.querySelector(".formContainer").appendChild(subBut);
+
+  const resBut = document.createElement("button");
+  resBut.setAttribute("class", "formButton");
+  resBut.setAttribute("id", "reset");
+  resBut.innerText = "Reset";
+  document.querySelector(".formContainer").appendChild(resBut);
+}
+
+const showName = () => {
+    const div =   document.querySelector(".formContainer");
+    while(div.firstChild){
+        div.removeChild(div.firstChild);
+    }
+    const nameInputLabel = document.createElement("label");
+  nameInputLabel.setAttribute("for", "plantName");
+  nameInputLabel.setAttribute("class", "label");
+  nameInputLabel.innerText = "Plant name:";
+  document.querySelector(".formContainer").appendChild(nameInputLabel);
+  document.querySelector(".formContainer").appendChild(document.createElement("br"));
+  const nameInput = document.createElement("input");
+  nameInput.setAttribute("type", "text");
+  nameInput.setAttribute("class", "input");
+  nameInput.setAttribute("id", "inputBoxActual");
+  nameInput.setAttribute("name", "plantName");
+  document.querySelector(".formContainer").appendChild(nameInput);
+  document.querySelector(".formContainer").appendChild(document.createElement("br"));
+
+  const subBut = document.createElement("button");
+    subBut.setAttribute("class", "formButton");
+    subBut.setAttribute("id", "submit");
+    subBut.innerText = "Submit";
+    document.querySelector(".formContainer").appendChild(subBut);
+  
+    const resBut = document.createElement("button");
+    resBut.setAttribute("class", "formButton");
+    resBut.setAttribute("id", "reset");
+    resBut.innerText = "Reset";
+    document.querySelector(".formContainer").appendChild(resBut);
+}
+
 buttons.forEach(btn => {
     btn.addEventListener('click', event => {
         if (event.target.innerText === "create") {
