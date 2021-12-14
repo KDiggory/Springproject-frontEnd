e22abb99f562742cb3f19958d9904852ed294437
Katie Diggory <Katie.diggory@gmail.com>
2021-12-14 10:56:34 +0000

    getting update function working

--- scripts/updateFunction.js
+++ scripts/updateFunction.js
@@ -3,6 +3,7 @@
 const buttonsUpdate = document.querySelectorAll(".button");
 
 const update = () => {
+
     const outputDiv =   document.querySelector(".outputcontainer");
     while(outputDiv.firstChild){
         outputDiv.removeChild(outputDiv.firstChild);
@@ -28,7 +29,7 @@ document.querySelector(".formContainer").appendChild(document.createElement("br"
 const nameInput = document.createElement("input");
 nameInput.setAttribute("type", "text");
 nameInput.setAttribute("class", "input");
-nameInput.setAttribute("id", "inputBoxActual");
+nameInput.setAttribute("id", "nameActual");
 nameInput.setAttribute("name", "plantName");
 document.querySelector(".formContainer").appendChild(nameInput);
 document.querySelector(".formContainer").appendChild(document.createElement("br"));
@@ -115,7 +116,113 @@ resBut.setAttribute("class", "formButton");
 resBut.setAttribute("id", "reset");
 resBut.innerText = "Reset";
 document.querySelector(".formContainer").appendChild(resBut);
+// add event listener for submit button
+
+document.querySelector(".formContainer").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const form = this;
+    const id = form.idActual.value;
+    const data = {
+        name: form.nameActual.value,
+        foliageColour: form.foliageColActual.value,
+        plantingMonth: form.monthActual.value,
+        plantingPosition: form.positionActual.value,
+        flowerColour: form.flowerActual.value,
+        id: form.idActual.value
+    }
+  console.log(data);
+  axios
+      .put(`${baseURLCreate}/updatePlant/${id}`, data)
+      .then(
+          console.log("in the update axios function, after put"))
+      .catch(err => console.error(err))    
+      readUpdate()
+},)
+// add event listener for reset button
+reset.addEventListener("click", () => {
+    showFormUpdate(); 
+  
+})
 }
+
+const readUpdate = () => {
+    const form = this;
+    const id = form.idActual.value;
+    const div =   document.querySelector(".formContainer");
+    while(div.firstChild){
+        div.removeChild(div.firstChild);
+    }
+    const outputDiv =   document.querySelector(".outputcontainer");
+    while(outputDiv.firstChild){
+        outputDiv.removeChild(outputDiv.firstChild);
+}
+    console.log("in the read function - after update");
+    document.querySelector("#doingWhat").textContent = "Reading all entries";
+    const output = document.createElement("h2");
+    output.setAttribute("class", "output-text")
+    document.querySelector(".outputcontainer").appendChild(output);
+
+    axios
+    .get(`${baseURLCreate}/getPlantById/${id}`)
+    .then(res => {
+        const plants = res.data;
+        
+            console.log(plants); // should log updated plant to console
+            const plantCol = document.createElement("div");
+            plantCol.setAttribute("class", "col");
+
+            const plantCard = document.createElement("div");
+            plantCard.setAttribute("class", "card");
+
+            const plantBody = document.createElement("div");
+            plantBody.setAttribute("class", "card-body");
+
+            const plantTitle = document.createElement("h2");
+            plantTitle.setAttribute("class", "card-title");
+            plantTitle.innerText = `${plants.name}`;
+            output.appendChild(plantTitle);
+
+            const plantFoliage = document.createElement("p");
+            plantFoliage.setAttribute("class", "card-text");
+            plantFoliage.innerText = `Foliage colour: ${plants.foliageColour}`;
+            output.appendChild(plantFoliage);
+
+            const plantMonth = document.createElement("p");
+            plantMonth.setAttribute("class", "card-text");
+            plantMonth.innerText = `Planting month: ${plants.plantingMonth}`;
+            output.appendChild(plantMonth);
+
+            const plantPosition = document.createElement("p");
+            plantPosition.setAttribute("class", "card-text");
+            plantPosition.innerText = `Planting position: ${plants.plantingPosition}`;
+            output.appendChild(plantPosition);
+
+            const plantFlower = document.createElement("p");
+            plantFlower.setAttribute("class", "card-text");
+            plantFlower.innerText = `Flower colour: ${plants.flowerColour}`;
+            output.appendChild(plantFlower);
+
+            const plantDel = document.createElement("button");
+            plantDel.innerText = "delete";
+            plantDel.classList.add("btn", "btn-danger");
+            plantDel.addEventListener("click", () => {
+                    axios
+                        .delete(`${baseURLCreate}/deletePlant/${plants.id}`)
+                        .then(
+                            readCreate())
+                        .catch(err => console.error(err))
+                        console.log(res);
+                        document.querySelector(".outputcontainer").innerText = "";
+                        readUpdate();
+        });
+        plantBody.appendChild(plantDel);
+        plantCard.appendChild(plantBody);
+        plantCol.appendChild(plantDel);
+        output.appendChild(plantCol);
+    })
+
+    }
+
 
 
 buttonsUpdate.forEach(btn => {
