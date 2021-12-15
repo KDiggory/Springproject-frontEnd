83d18782febdaf59f923ad227c99a6900f8ed137
Katie Diggory <Katie.diggory@gmail.com>
2021-12-15 12:52:27 +0000

    removed get function in update

--- scripts/updateFunction.js
+++ scripts/updateFunction.js
@@ -141,74 +141,76 @@ reset.addEventListener("click", () => {
 
 const readUpdated = () => {
     clear();
-    const output = document.createElement("h2");
-    output.setAttribute("class", "output-text")
-    document.querySelector(".outputcontainer").appendChild(output);
-    axios
-    .get(`${baseURLUpdate}/getAll/`)
-    .then(res => { 
-        const plants = res.data;
-        console.log(plants);
-        for(let i = 0; i<plants.length; i++){
-            if (i.id === lastupdated){
-            const plantCol = document.createElement("div");
-            plantCol.setAttribute("class", "row row-cols-2 row-cols-md-4 g-4");
+    document.querySelector("#doingWhat").textContent = "Updated!";
+    
+    // const output = document.createElement("h2");
+    // output.setAttribute("class", "output-text")
+    // document.querySelector(".outputcontainer").appendChild(output);
+    // axios
+    // .get(`${baseURLUpdate}/getAll/`)
+    // .then(res => { 
+    //     const plants = res.data;
+    //     console.log(plants);
+    //     for(let i = 0; i<plants.length; i++){
+    //         if (i.id === lastupdated){
+    //         const plantCol = document.createElement("div");
+    //         plantCol.setAttribute("class", "row row-cols-2 row-cols-md-4 g-4");
 
-            const plantCard = document.createElement("div");
-            plantCard.setAttribute("class", "card");
+    //         const plantCard = document.createElement("div");
+    //         plantCard.setAttribute("class", "card");
 
-            const plantBody = document.createElement("div");
-            plantBody.setAttribute("class", "card-body");
+    //         const plantBody = document.createElement("div");
+    //         plantBody.setAttribute("class", "card-body");
 
-            const plantTitle = document.createElement("h3");
-            plantTitle.setAttribute("class", "card-title");
-            plantTitle.innerText = `${plants[i].name}`;
-            output.appendChild(plantTitle);
+    //         const plantTitle = document.createElement("h3");
+    //         plantTitle.setAttribute("class", "card-title");
+    //         plantTitle.innerText = `${plants[i].name}`;
+    //         output.appendChild(plantTitle);
 
-            const plantFoliage = document.createElement("p");
-            plantFoliage.setAttribute("class", "card-text");
-            plantFoliage.innerText = `Foliage colour: ${plants[i].foliageColour}`;
-            output.appendChild(plantFoliage);
+    //         const plantFoliage = document.createElement("p");
+    //         plantFoliage.setAttribute("class", "card-text");
+    //         plantFoliage.innerText = `Foliage colour: ${plants[i].foliageColour}`;
+    //         output.appendChild(plantFoliage);
 
-            const plantMonth = document.createElement("p");
-            plantMonth.setAttribute("class", "card-text");
-            plantMonth.innerText = `Planting month: ${plants[i].plantingMonth}`;
-            output.appendChild(plantMonth);
+    //         const plantMonth = document.createElement("p");
+    //         plantMonth.setAttribute("class", "card-text");
+    //         plantMonth.innerText = `Planting month: ${plants[i].plantingMonth}`;
+    //         output.appendChild(plantMonth);
 
-            const plantPosition = document.createElement("p");
-            plantPosition.setAttribute("class", "card-text");
-            plantPosition.innerText = `Planting position: ${plants[i].plantingPosition}`;
-            output.appendChild(plantPosition);
+    //         const plantPosition = document.createElement("p");
+    //         plantPosition.setAttribute("class", "card-text");
+    //         plantPosition.innerText = `Planting position: ${plants[i].plantingPosition}`;
+    //         output.appendChild(plantPosition);
 
-            const plantFlower = document.createElement("p");
-            plantFlower.setAttribute("class", "card-text");
-            plantFlower.innerText = `Flower colour: ${plants[i].flowerColour}`;
-            output.appendChild(plantFlower);
+    //         const plantFlower = document.createElement("p");
+    //         plantFlower.setAttribute("class", "card-text");
+    //         plantFlower.innerText = `Flower colour: ${plants[i].flowerColour}`;
+    //         output.appendChild(plantFlower);
 
-            const plantId = document.createElement("p");
-            plantId.setAttribute("class", "card-text");
-            plantId.innerText = `ID: ${plants[i].id}`;
-            output.appendChild(plantId);
+    //         const plantId = document.createElement("p");
+    //         plantId.setAttribute("class", "card-text");
+    //         plantId.innerText = `ID: ${plants[i].id}`;
+    //         output.appendChild(plantId);
 
-            const plantDel = document.createElement("button");
-            plantDel.setAttribute("id", "cardButton");
-            plantDel.innerText = "delete";
-            plantDel.addEventListener("click", () => {
-                    axios
-                        .delete(`${baseURLUpdate}/deletePlant/${plants[i].id}`)
-                        .then(
-                            readCreate())
-                        .catch(err => console.error(err))
-                        console.log(res);
-                        document.querySelector(".outputcontainer").innerText = "";
-                        readUpdated();
-        });
-        plantBody.appendChild(plantDel);
-        plantCard.appendChild(plantBody);
-        plantCol.appendChild(plantDel);
-        output.appendChild(plantCol);
-    }}
-    })
+    //         const plantDel = document.createElement("button");
+    //         plantDel.setAttribute("id", "cardButton");
+    //         plantDel.innerText = "delete";
+    //         plantDel.addEventListener("click", () => {
+    //                 axios
+    //                     .delete(`${baseURLUpdate}/deletePlant/${plants[i].id}`)
+    //                     .then(
+    //                         readCreate())
+    //                     .catch(err => console.error(err))
+    //                     console.log(res);
+    //                     document.querySelector(".outputcontainer").innerText = "";
+    //                     readUpdated();
+    //     });
+    //     plantBody.appendChild(plantDel);
+    //     plantCard.appendChild(plantBody);
+    //     plantCol.appendChild(plantDel);
+    //     output.appendChild(plantCol);
+    // }}
+    // })
 }
 
 buttonsUpdate.forEach(btn => {
